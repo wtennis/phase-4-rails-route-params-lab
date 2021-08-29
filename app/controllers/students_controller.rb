8a8808a36d9e5d136564e991b2f1edd51a7bc0a4
Whiting <whitinghtennis@gmail.com>
2021-08-29 12:37:39 -0700

    commit tree 3e8f5b85495ea39aabda0ab1a6d15ce537602926

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,15 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show 
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  def bonus
+    student = Student.all.find_by(first_name: params[:value1], last_name: params[:value2])
+    render json: student
+  end
+
+
 end
